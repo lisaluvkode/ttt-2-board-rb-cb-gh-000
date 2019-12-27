58d7ae174fcd3ae7aba96db0ad86ea99498fe074
Lisa Nguyen <lisan0182@att.net>
2019-12-27 19:20:12 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,1 +1,1 @@
-board = ["one","two","three","four","five","six","seven","eight","nine"]# Define the variable board below.
+board = [" "," "," ",""," "," "," "," "," "]# Define the variable board below.
